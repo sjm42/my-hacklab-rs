b8a7b15751a19d18a0bc5974fd83437b179ce9f7
Sami J. Mäkinen <sjm@iki.fi>
2021-08-25 20:24:44 +0300

    fmt ftw

--- src/main.rs
+++ src/main.rs
@@ -11,7 +11,7 @@ use utils::spd3303x;
 
 use crate::utils::{sdl1000x::SDL1000X, spd3303x::SPD3303X};
 
-const LAB_LOAD: &str = "lab-LOAD.siu.ro:5025";
+const LAB_LOAD: &str = "lab-load.siu.ro:5025";
 const LAB_POWER: &str = "lab-power.siu.ro:5025";
 
 #[derive(Debug, Clone, StructOpt)]
@@ -63,20 +63,36 @@ async fn main() -> Result<(), Box<dyn Error>> {
     info!("LOAD function? {}", &ll.req("func?").await?);
     info!("LOAD sense? {}", &ll.get_state("syst:sens").await?);
 
-    info!("LOAD short SET {}", &ll.set_state(":shor", sdl1000x::State::Off).await?);
-    info!("LOAD input SET {}", &ll.set_state(":inp", sdl1000x::State::Off).await?);
-    info!("LOAD sense SET {}", &ll.set_state("syst:sens", sdl1000x::State::On).await?);
+    info!(
+        "LOAD short SET {}",
+        &ll.set_state(":shor", sdl1000x::State::Off).await?
+    );
+    info!(
+        "LOAD input SET {}",
+        &ll.set_state(":inp", sdl1000x::State::Off).await?
+    );
+    info!(
+        "LOAD sense SET {}",
+        &ll.set_state("syst:sens", sdl1000x::State::On).await?
+    );
 
-    info!("PWR track 0"); lp.send("output:track 0").await?;
-    info!("PWR ch1 off"); lp.send("output ch1,off").await?;
-    info!("PWR ch2 off"); lp.send("output ch2,off").await?;
-    info!("PWR ch3 off"); lp.send("output ch3,off").await?;
-    info!("PWR wave ch1 off"); lp.send("output:wave ch1,off").await?;
-    info!("PWR wave ch2 off"); lp.send("output:wave ch2,off").await?;
+    info!("PWR track 0");
+    lp.send("output:track 0").await?;
+    info!("PWR ch1 off");
+    lp.send("output ch1,off").await?;
+    info!("PWR ch2 off");
+    lp.send("output ch2,off").await?;
+    info!("PWR ch3 off");
+    lp.send("output ch3,off").await?;
+    info!("PWR wave ch1 off");
+    lp.send("output:wave ch1,off").await?;
+    info!("PWR wave ch2 off");
+    lp.send("output:wave ch2,off").await?;
 
     info!("PWR ch1 volt SET {}", &lp.set("ch1:volt", 4.250).await?);
     info!("PWR ch1 curr SET {}", &lp.set("ch1:curr", 0.250).await?);
-    info!("PWR ch1 SET on"); lp.send("output ch1,on").await?;
+    info!("PWR ch1 SET on");
+    lp.send("output ch1,on").await?;
 
     info!("LOAD input? {}", &ll.req(":inp?").await?);
     info!("LOAD short? {}", &ll.req(":shor?").await?);
@@ -86,13 +102,19 @@ async fn main() -> Result<(), Box<dyn Error>> {
     info!("***");
     thread::sleep(time::Duration::new(1, 0));
 
-    info!("LOAD func SET {}", &ll.set_func(sdl1000x::Func::Curr).await?);
+    info!(
+        "LOAD func SET {}",
+        &ll.set_func(sdl1000x::Func::Curr).await?
+    );
     // 5A or 30A
     info!("LOAD irange SET {}", &ll.set(":curr:irang", 5.0).await?);
     // 36V or 150V
     info!("LOAD vrange SET {}", &ll.set(":curr:vrang", 36.0).await?);
     info!("LOAD current SET {:.3}", &ll.set(":curr", 0.120).await?);
-    info!("LOAD input SET {}", &ll.set_state(":inp", sdl1000x::State::On).await?);
+    info!(
+        "LOAD input SET {}",
+        &ll.set_state(":inp", sdl1000x::State::On).await?
+    );
 
     info!("***");
     thread::sleep(time::Duration::new(2, 0));
@@ -103,9 +125,18 @@ async fn main() -> Result<(), Box<dyn Error>> {
     info!("LOAD power? {:.3}", &ll.meas(sdl1000x::Meas::Pow).await?);
     info!("LOAD res? {:.3}", &ll.meas(sdl1000x::Meas::Res).await?);
 
-    info!("PWR ch1 volt? {:.3}", &lp.meas(spd3303x::Ch::Ch1, spd3303x::Meas::Volt).await?);
-    info!("PWR ch1 curr? {:.3}", &lp.meas(spd3303x::Ch::Ch1, spd3303x::Meas::Curr).await?);
-    info!("PWR ch1 power? {:.3}", &lp.meas(spd3303x::Ch::Ch1, spd3303x::Meas::Pow).await?);
+    info!(
+        "PWR ch1 volt? {:.3}",
+        &lp.meas(spd3303x::Ch::Ch1, spd3303x::Meas::Volt).await?
+    );
+    info!(
+        "PWR ch1 curr? {:.3}",
+        &lp.meas(spd3303x::Ch::Ch1, spd3303x::Meas::Curr).await?
+    );
+    info!(
+        "PWR ch1 power? {:.3}",
+        &lp.meas(spd3303x::Ch::Ch1, spd3303x::Meas::Pow).await?
+    );
     info!("***");
     info!("PWR ch1 volt SET {}", &lp.set("ch1:volt", 8.500).await?);
     info!("LOAD current SET {:.3}", &ll.set(":curr", 0.150).await?);
@@ -117,16 +148,32 @@ async fn main() -> Result<(), Box<dyn Error>> {
     info!("LOAD curr? {:.3}", &ll.meas(sdl1000x::Meas::Curr).await?);
     info!("LOAD power? {:.3}", &ll.meas(sdl1000x::Meas::Pow).await?);
     info!("LOAD res? {:.3}", &ll.meas(sdl1000x::Meas::Res).await?);
-    info!("PWR ch1 volt? {:.3}", &lp.meas(spd3303x::Ch::Ch1, spd3303x::Meas::Volt).await?);
-    info!("PWR ch1 curr? {:.3}", &lp.meas(spd3303x::Ch::Ch1, spd3303x::Meas::Curr).await?);
-    info!("PWR ch1 power? {:.3}", &lp.meas(spd3303x::Ch::Ch1, spd3303x::Meas::Pow).await?);
+    info!(
+        "PWR ch1 volt? {:.3}",
+        &lp.meas(spd3303x::Ch::Ch1, spd3303x::Meas::Volt).await?
+    );
+    info!(
+        "PWR ch1 curr? {:.3}",
+        &lp.meas(spd3303x::Ch::Ch1, spd3303x::Meas::Curr).await?
+    );
+    info!(
+        "PWR ch1 power? {:.3}",
+        &lp.meas(spd3303x::Ch::Ch1, spd3303x::Meas::Pow).await?
+    );
 
     info!("***");
     thread::sleep(time::Duration::new(1, 0));
 
-    info!("LOAD input SET {}", &ll.set_state(":inp", sdl1000x::State::Off).await?);
-    info!("LOAD sense SET {}", &ll.set_state("syst:sens", sdl1000x::State::Off).await?);
-    info!("PWR ch1 SET off"); lp.send("output ch1,off").await?;
+    info!(
+        "LOAD input SET {}",
+        &ll.set_state(":inp", sdl1000x::State::Off).await?
+    );
+    info!(
+        "LOAD sense SET {}",
+        &ll.set_state("syst:sens", sdl1000x::State::Off).await?
+    );
+    info!("PWR ch1 SET off");
+    lp.send("output ch1,off").await?;
 
     Ok(())
 }
